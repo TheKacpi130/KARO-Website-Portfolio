25c096dd1c189e75eaafe332a63f224592f9c60b
TheKacpi130 <thekacpi130@gmail.com>
2023-02-05 00:00:37 +0100

    sobotni wieczór :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/* # SHOW MENU # */
+/* #### SHOW MENU #### */
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,17 +11,50 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/* # REMOVE MENU MOBILE #*/
+/* #### REMOVE MENU MOBILE #### */
+const navLink = document.querySelectorAll('.nav__link')
 
+    function linkAction(){
+        const navMenu = document.getElementById('nav-menu')
+        navMenu.classList.remove('show-menu')
+    }
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/* # SCROLL SECTIONS ACTIVE LINK # */
+/* #### SCROLL SECTIONS ACTIVE LINK #### */
+const sections = document.querySelectorAll ('section [id]')
 
+function scrollActive () {
+    const scrollY = window.pageYOffset
 
-/* # CHANGE BACKGROUND HEADER # */ 
+    sections.forEach (current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute ('id')
 
+        if (scrollY> sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector ('. nav__menu a [href * =' + sectionId + ']'). classList.add ('active-link')
+        } else {
+            document.querySelector ('. nav__menu a [href * =' + sectionId + ']'). classList.remove ('active-link')
+        }
+    })
+}
+window.addEventListener ('scroll', scrollActive)
 
-/* # SHOW SCROLL TOP # */ 
+/* #### CHANGE BACKGROUND HEADER #### */ 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
 
+/* #### SHOW SCROLL TOP #### */ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop)
 
 /* # MIXITUP FILTER PORTFOLIO # */ 
 
