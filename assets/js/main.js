0f22624fa532054775460ed23cb2f3f51b242c75
TheKacpi130 <thekacpi130@gmail.com>
2023-02-05 13:03:34 +0100

    >About

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,20 +21,20 @@ const navLink = document.querySelectorAll('.nav__link')
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* #### SCROLL SECTIONS ACTIVE LINK #### */
-const sections = document.querySelectorAll ('section [id]')
+const sections = document.querySelectorAll ('section[id]')
 
-function scrollActive () {
+function scrollActive(){
     const scrollY = window.pageYOffset
 
-    sections.forEach (current => {
+    sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute ('id')
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
 
-        if (scrollY> sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector ('. nav__menu a [href * =' + sectionId + ']'). classList.add ('active-link')
-        } else {
-            document.querySelector ('. nav__menu a [href * =' + sectionId + ']'). classList.remove ('active-link')
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector ('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else{
+            document.querySelector ('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
@@ -43,16 +43,14 @@ window.addEventListener ('scroll', scrollActive)
 /* #### CHANGE BACKGROUND HEADER #### */ 
 function scrollHeader(){
     const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader)
 
 /* #### SHOW SCROLL TOP #### */ 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top')
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
-    else scrollTop.classList.remove('show-scroll');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollTop)
 
